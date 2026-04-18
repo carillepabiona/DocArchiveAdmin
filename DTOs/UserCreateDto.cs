55de2a369dc4f0e2617bb2e9fa568c5a43da0a04
Carille Pabiona <carillepabiona49@gmail.com>
2026-04-19 02:49:44 +0800

    admin only

--- DTOs/UserCreateDto.cs
+++ DTOs/UserCreateDto.cs
@@ -2,9 +2,16 @@
 {
     public class UserCreateDto
     {
+        public string UserCode { get; set; }      // NEW
         public string Username { get; set; }
-        public string Password { get; set; }   // plain password (API will hash)
+        public string Password { get; set; }
+
         public string FullName { get; set; }
+
+        public string Email { get; set; }         // NEW
+        public string ContactNumber { get; set; } // NEW
+        public string Address { get; set; }       // NEW
+
         public string Role { get; set; }
         public bool IsActive { get; set; }
         public bool ForcePasswordChangeOnLogin { get; set; }
